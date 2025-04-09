e4415e5041e91fb4e2e9f0555477c642de67bf4a
jeffersondlmatta <jeffersondlmatta@gmail.com>
2025-04-08 22:56:26 -0300

    atualização de documentação de api

--- src/main/java/br/javaAngular/JavaWithAngularApplication.java
+++ src/main/java/br/javaAngular/JavaWithAngularApplication.java
@@ -1,8 +1,19 @@
 package br.javaAngular;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Documento API - cadastro de pessoas",
+                version = "1.0",
+                description = "Documentando uma API básica de gerenciamento de pessoas",
+                contact = @Contact(name = "Jeff", email = "jeffersondlmatta@gmail.com", url = "https://github.com/jeffersondlmatta")
+        )
+)
 @SpringBootApplication
 public class JavaWithAngularApplication {
 
--- src/main/java/br/javaAngular/controller/Controller.java
+++ src/main/java/br/javaAngular/controller/Controller.java
@@ -2,9 +2,17 @@ package br.javaAngular.controller;
 
 
 import br.javaAngular.model.Cliente;
+import br.javaAngular.model.Mensagem;
 import br.javaAngular.repository.Repositorio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -13,25 +21,63 @@ public class Controller {
     @Autowired
     private Repositorio acao;
 
-    @PostMapping("/")
+    @PostMapping("/cadastrar")
+    @Operation(summary = "Rota responsável pelo cadastro de pessoas")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Pessoa cadastrada com sucesso",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Cliente.class)
+                            )
+                    }
+            ),
+
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Informação inválida",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Mensagem.class)
+                            )
+                    }
+            )
+    })
     public Cliente cadastrar(@RequestBody Cliente c) {
-
         return acao.save(c);
+    }
+
+    @GetMapping("/listar")
+    @Operation(summary = "Rota responsável pelo listagem de pessoas")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Listagem de pessoas",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Cliente.class)
+                            )
+                    }
+            )
+    })
+    public List<Cliente> listarPessoas(){
+
+        // Retornar a lista de pessoas cadastradas
+        return this.acao.findAll();
 
     }
 
-    @GetMapping("/")
-    public Iterable<Cliente> selecionar()
-    {
-       return acao.findAll();
-    }
 
-    @PutMapping("/")
+    @PutMapping("/editar")
     public Cliente editar(@RequestBody Cliente c){
         return acao.save(c);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/deletar/{id}")
     public void remover(@PathVariable long id){
         acao.deleteById(id);
     }
